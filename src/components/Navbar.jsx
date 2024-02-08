56d4f2b439d21aef1f14cce3cb49153ac6d32578
Tom <twallace1015@gmail.com>
2024-02-07 19:57:21 -0500

    finished navbar component. styled for both small and large screens

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,9 @@ import { close, logo, menu } from "../assets";
 import { navLinks } from "../constants";
 
 const Navbar = () => {
+  // using useState hook to set the toggle of hamburger menu icon
+  // Map over navlink array in our constants file to get our nav links. Use iternary statements to determine if they are styled with hamburger icon for smaller screens or without for larger
+  const [toggle, setToggle] = useState(false);
   return (
     <nav className='w-full flex py-6 justify-between items-center navbar'>
       <img src={logo} alt='hookbank' className='w-[124px] h-[32px]' />
@@ -18,6 +21,31 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
+
+      <div className='sm:hidden flex flex-1 justify-end items-center'>
+        <img
+          src={toggle ? close : menu}
+          alt='menu'
+          className='w-[28px] h-[28px] object-contain'
+          onClick={() => setToggle((prev) => !prev)}
+        />
+        <div
+          className={`${
+            toggle ? "flex" : "hidden"
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar`}>
+          <ul className='list-none flex flex-col justify-end items-center flex-1'>
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins font-normal cursor-pointer text-[16px] ${
+                  index === navLinks.length - 1 ? "mr-0" : "mb-4"
+                } text-white`}>
+                <a href={`#${nav.id}`}>{nav.title} </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 };
